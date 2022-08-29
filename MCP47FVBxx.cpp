ad7ce2ae5c7906bd20a3633e800f8c5ae2213823
Ali Akmal <76255471+22441@users.noreply.github.com>
2022-08-29 10:09:45 +0200

    Update MCP47FVBxx.cpp

--- MCP47FVBxx.cpp
+++ MCP47FVBxx.cpp
@@ -1,3 +1,19 @@
+/* Copyright 2022 Ali Akmal
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   */  
+     
+     
 #include "MCP47FVBxx.h"
 
 MCP47FVBxx::MCP47FVBxx(uint8_t no_of_channels, uint8_t Resolution, uint8_t A1A0_State, float VRefVolt ) { 
